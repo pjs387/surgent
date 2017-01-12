89dd83dffd1fac9abace4dc227e78cff89427e60
Phil <pjs387@users.noreply.github.com>
2017-01-12 16:51:05 -0500

    Add files via upload

---
+++ cpa-review/pages/FreeTrialForm.js
@@ -0,0 +1,93 @@
+function submitTrialForm2() {
+	if ("" == $("#ctl00_ContentPlaceHolder1_C006_ctl00_ctl00_ctl00_C006_ctl00_ctl00_txtText").val() || 
+		"" == $("#ctl00_ContentPlaceHolder1_C006_ctl00_ctl00_ctl00_C007_ctl00_ctl00_txtText").val() || 
+		"" == $("#ctl00_ContentPlaceHolder1_C006_ctl00_ctl00_ctl00_C008_ctl00_ctl00_txtText").val() || 
+		"" == $("#ctl00_ContentPlaceHolder1_C006_ctl00_ctl00_ctl00_C009_ctl00_ctl00_txtText").val())
+	{
+		return;
+	}
+		
+	
+	var fr = document.createElement("iframe");
+	document.body.appendChild(fr);
+	//fr.style.display = "none";
+	fr.contentWindow.name = "trialFormSubmitHiddenIframe2";
+	
+	var frm = document.createElement("form");
+	frm.action = "https://review.surgent.com/Pass/TrialSignup.aspx";
+	frm.method = "post";
+	frm.id = "TrialSignupForm";
+	//frm.style.display = "none";
+	frm.target = "trialFormSubmitHiddenIframe2";
+	
+	var pr = document.createElement("input");
+	pr.type = "text";
+	pr.name = "ProductTextBox";
+	pr.id = "ProductTextBox";
+	pr.setAttribute("value", "CPA");
+	frm.appendChild(pr);
+	
+	var fn = document.createElement("input");
+	fn.type = "text";
+	fn.name = "FirstNameTextBox";
+	fn.id = "FirstNameTextBox";
+	fn.setAttribute("value", $("#ctl00_ContentPlaceHolder1_C006_ctl00_ctl00_ctl00_C014_ctl00_ctl00_txtText").val());
+	frm.appendChild(fn);
+	
+	var ln = document.createElement("input");
+	ln.type = "text";
+	ln.name = "LastNameTextBox";
+	ln.id = "LastNameTextBox";
+	ln.setAttribute("value", $("#ctl00_ContentPlaceHolder1_C006_ctl00_ctl00_ctl00_C015_ctl00_ctl00_txtText").val());
+	frm.appendChild(ln);
+	
+	var em = document.createElement("input");
+	em.type = "text";
+	em.name = "EmailTextBox";
+	em.id = "EmailTextBox";
+	em.setAttribute("value", $("#ctl00_ContentPlaceHolder1_C006_ctl00_ctl00_ctl00_C016_ctl00_ctl00_txtText").val());
+	frm.appendChild(em);
+	
+	var ph = document.createElement("input");
+	ph.type = "text";
+	ph.name = "PhoneTextBox";
+	ph.id = "PhoneTextBox";
+	ph.setAttribute("value", $("#ctl00_ContentPlaceHolder1_C006_ctl00_ctl00_ctl00_C028_ctl00_ctl00_txtText").val());
+	frm.appendChild(ph);
+	
+	document.body.appendChild(frm);
+	
+	console.log("here");
+	console.log($(frm).html());
+	console.log($(frm).serialize());
+	
+	$.ajax({
+		url: "https://review.surgent.com/Pass/TrialSignup.aspx",
+		data: $(frm).serialize(),
+		//dataType: "jsonp",
+		type: 'POST',
+		success: function (res) {
+			console.log("success");
+		},
+		error: function(xhr, text, ex1) {
+			console.log(xhr);
+			console.log(text);
+			console.log(ex1);
+		},
+		complete: function(xhr, text) {
+			console.log(xhr);
+			console.log(text);
+		}
+	}); 
+	
+	//frm.submit();
+	//ga("send", "event", "Submit", "click", "CPA Review Submit Free Trial");
+	//window.uetq = window.uetq || [];
+	//window.uetq.push({
+	//	ec: "Submit",
+	//	ea: "On Click",
+	//	el: "CPA Review Free Trial",
+	//	ev: 0
+	//});
+	//fbq("track", "CompleteRegistration");
+}
